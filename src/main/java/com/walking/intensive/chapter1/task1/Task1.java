a9a7f841604084ba4cf2d90d54b25380d07d9624
ruslan <beamingrus@gmail.com>
2024-11-18 00:31:49 +0300

    Task1 на проверку от 18.11.24  EvgeneZhurov "Просьба начать с самого начала без try, циклов и прочих ненужных для решения задачи вещей. Метод должен возвращать строку согласно условию задачи, ввода с клавиатуры не требуется."

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,5 +1,4 @@
 package com.walking.intensive.chapter1.task1;
-import java.util.Scanner;
 /**
  * Реализуйте метод getAgeString(), который будет принимать параметром целое число (возраст) и возвращать строку
  * вида: "Вам N лет". Программа должна учитывать правила русского языка.
@@ -16,52 +15,30 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 21;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
+        String sYears; //итоговая строка
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введи возраст");
-        do
-        {
-            try { //  Убеждаемся что в пользовательском вводе введен int а не что-то другое
-                String s = in.nextLine();
-                String sYears; //итоговая строка
-                int midterm = Integer.parseInt(s) ;
-                if (midterm % 100 >= 11 && midterm % 100 <= 14) {
-                    sYears = "вам " + (midterm) + " лет";
-
-                }
-                else {
-                    int ageLastNumber = midterm % 10;
-                    if (ageLastNumber == 1) {
-                            sYears = "вам " + (midterm) + " год";
-                        }
-                        else if(ageLastNumber == 0 || ageLastNumber >= 5 && ageLastNumber <= 9) {
-                                sYears = "вам " + (midterm) + " лет";
-                            }
-                        else if(ageLastNumber >= 2 && ageLastNumber <= 4) {
-                                sYears = "вам " + (midterm) + " года";
-                            }
-
-                        else sYears = "вам " + (midterm) + " лет";
+        if (age % 100 >= 11 && age % 100 <= 14) {
+            sYears = "вам " + (age) + " лет";
+        } else {
+            int ageLastNumber = age % 10;
+            if (ageLastNumber == 1) {
+                sYears = "вам " + (age) + " год";
+            } else if (ageLastNumber == 0 || ageLastNumber >= 5 && ageLastNumber <= 9) {
+                sYears = "вам " + (age) + " лет";
+            } else if (ageLastNumber >= 2 && ageLastNumber <= 4) {
+                sYears = "вам " + (age) + " года";
+            } else sYears = "вам " + (age) + " лет";
 
 
-                }
-                System.out.println(sYears);
-
-            }
-            catch (Exception e)
-                {
-                    System.out.println("Некорректный ввод");
-                }
         }
-        while (true); // итоговое поведение не формализовано - так что бесконечно опрашиваем возраст
 
-//git test
 
+        return sYears;
     }
 }
