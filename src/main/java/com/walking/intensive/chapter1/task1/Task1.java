5dab365321bfcc8d423220c35eaeb33dfcbf85bc
ruslan <beamingrus@gmail.com>
2024-11-20 13:21:25 +0300

    Поправил в соответствии с автотестами

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,4 +1,5 @@
 package com.walking.intensive.chapter1.task1;
+
 /**
  * Реализуйте метод getAgeString(), который будет принимать параметром целое число (возраст) и возвращать строку
  * вида: "Вам N лет". Программа должна учитывать правила русского языка.
@@ -15,30 +16,37 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 21;
 
-        System.out.println(getAgeString(age));
+
+
     }
 
     static String getAgeString(int age) {
         String sYears; //итоговая строка
+        int ageLastNumber = age % 10;
 
-        if (age % 100 >= 11 && age % 100 <= 14) {
-            sYears = "вам " + (age) + " лет";
+
+        if (age < 0) {
+            sYears = "Некорректный ввод";
+        } else if (age % 100 >= 11 && age % 100 <= 14) {
+            sYears = "Вам " + (age) + " лет";
+
+        } else if (ageLastNumber == 1) {
+            sYears = "Вам " + (age) + " год";
+
+        } else if (ageLastNumber == 0) {
+            sYears = "Вам " + (age) + " лет";
+
+        } else if (ageLastNumber >= 5 && ageLastNumber <= 9) {
+            sYears = "Вам " + (age) + " лет";
+
+        } else if (ageLastNumber >= 2 && ageLastNumber <= 4) {
+            sYears = "Вам " + (age) + " года";
+
         } else {
-            int ageLastNumber = age % 10;
-            if (ageLastNumber == 1) {
-                sYears = "вам " + (age) + " год";
-            } else if (ageLastNumber == 0 || ageLastNumber >= 5 && ageLastNumber <= 9) {
-                sYears = "вам " + (age) + " лет";
-            } else if (ageLastNumber >= 2 && ageLastNumber <= 4) {
-                sYears = "вам " + (age) + " года";
-            } else sYears = "вам " + (age) + " лет";
-
-
+            sYears = "Вам " + (age) + " лет";
         }
-
-
         return sYears;
     }
+
 }
